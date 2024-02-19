1dba5e4806b5fa3a3b325a3761ee6afd5459194d
ZHANG Haoming <haoming.zhang@etu.unicaen.fr>
2024-02-19 15:07:54 +0100

    test2

--- SAE.py
+++ SAE.py
@@ -1,3 +1,9 @@
 import pandas as pd
+import numpy as np
 
 
+#Importer les données
+df_client = pd.read_csv('clients_greendwell.csv')
+df_service = pd.read_excel('historique_acces_greendwell.xlsx', sheet_name='Services Consommes')
+
+test
