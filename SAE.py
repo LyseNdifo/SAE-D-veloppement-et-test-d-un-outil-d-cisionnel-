81fbf78c05115933a7387eba21ccafcad5a5ab38
Lyse Priscille Ngasseu Ndifo <ngasseu231@campus.unicaen.fr>
2024-02-19 15:11:08 +0100

    test 2 de branches

--- SAE.py
+++ SAE.py
@@ -1,3 +1,21 @@
 import pandas as pd
+#import numpy as np
+#import matplotlib.pyplot as plt
+
+
+
+#chargement des données
+
+client = pd.read_csv('clients_greendwell.csv')
+historiques = pd.read_excel('historique_acces_greendwell.xlsx')
+
+
+#Conversion en data frames de données chargées
+client = pd.DataFrame(client)
+historiques = pd.DataFrame(historiques)
+
+print(client.head())
+
+print(historiques.head())
 
 
