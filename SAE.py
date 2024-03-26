d044ff3ab69be6cc602abf6b11d77284fadf99f4
LyseNdifo <lysendifo8l@gmail.com>
2024-03-26 14:22:56 +0100

    Supression

--- SAE.py
+++ SAE.py
@@ -5,5 +5,3 @@ import numpy as np
 #Importer les données
 df_client = pd.read_csv('clients_greendwell.csv')
 df_service = pd.read_excel('historique_acces_greendwell.xlsx', sheet_name='Services Consommes')
-
-test
